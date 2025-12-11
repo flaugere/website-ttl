c565d7e82bf4bf4071fb6cdc0ce57685768d7711
David FLAUGERE <flaugere.david@gmail.com>
2025-12-11 15:19:33 +0100

    feat(shop): remove unused card

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,8 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 
-// Import the Vercel adapter
-import vercel from '@astrojs/vercel';
-
 // https://astro.build/config
 export default defineConfig({
-  site: "https://Trailthelimittheme.vercel.app",
-  integrations: [tailwind()],
-  output: 'server',
-  adapter: vercel(),
+  site: "https://trailthelimit.fr",
+  integrations: [tailwind()]
 });
